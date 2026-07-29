1101f53883904dae95e8dd6a45396ab04fefc55d
Raven Paragas <ravenp.dev@gmail.com>
2026-07-29 13:42:46 +0800

    initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+// import { dirname } from "node:path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // turbopack: {
+  //   root: dirname(__filename),
+  // },
 };
 
 export default nextConfig;
